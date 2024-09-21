6f4dfce9805bc06d0d63e89638fb5e62a80ed62e
ONPRAE <onwarapat@gmail.com>
2024-09-21 10:29:09 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {  RouterView } from 'vue-router'
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          MiniStore
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/" label="Home" />
+        <q-route-tab to="/create" label="create" />
+        <q-route-tab to="/About" label="About" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+
